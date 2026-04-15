abb825651ca887ef9eb7b2bb657aedca485a7b1c
Generous <100401135+defineint@users.noreply.github.com>
2026-04-16 01:31:14 +0800

    fix driver options
    加入 Service 物件，並顯式關閉開發者模式檢測

--- anime_BOT.py
+++ anime_BOT.py
@@ -30,6 +30,9 @@ def get_driver(url):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
+
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
         
         driver = webdriver.Chrome(options=chrome_options)
         driver.get(url)
